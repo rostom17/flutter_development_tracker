d202ff18e41b6cba1124287b02c0724166ea6379
rostom17 <rostom825@gmail.com>
2025-04-10 20:14:15 +0600

    Image Picker Updated

--- lib/ImagePicker/main.dart
+++ lib/ImagePicker/main.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:image_picker_for_web/image_picker_for_web.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,6 +24,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  XFile? _image;
+  File? pickedImage;
+
+  Future<void> onTapTakePhoto() async {
+    _image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (_image != null) {
+      setState(() {
+        pickedImage = File(_image!.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +49,41 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image:
+                    pickedImage != null
+                        ? DecorationImage(image: FileImage(pickedImage!))
+                        : null,
+                color: Colors.amber,
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(300, 80),
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              onPressed: onTapTakePhoto,
+              child: Text(
+                "Take Photo",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
